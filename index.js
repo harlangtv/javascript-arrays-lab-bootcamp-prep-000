9ba599cca1e61e12c5146dc1c26e988949f21cc7
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-23 20:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
-function removeLastKitten(){
+function removeFirstKitten(){
   return kittens.slice(1)
 }
 
